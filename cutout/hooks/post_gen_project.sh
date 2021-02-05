44f5b08cf8c7881469965f3580b0c540e8c6ced0
Amanibhavam <iam@defn.sh>
2021-02-04 19:30:22 -0800

    diff

--- cutout/hooks/post_gen_project.sh
+++ cutout/hooks/post_gen_project.sh
@@ -4,12 +4,12 @@ function main {
   set -efu
   find static -type f | while read -r f; do sed -i '' "s#defn#$(cat .project_org)#g; s#podinfo#$(cat .project_name)#g" "$f"; done
   pushd static
-  mv charts/podinfo "charts/$(cat ../.project_name)"
-  mv cmd/podinfo "cmd/$(cat ../.project_name)"
+  mv charts/podinfo "charts/$(cat ../.project_name)" || true
+  mv cmd/podinfo "cmd/$(cat ../.project_name)" || true
   popd
   rsync -ia static/. "$(cat .project_name)/."
   rm -rf static
-  rm -f "$(cat .project_name)"/.project_{org,name}
+  rm -f .project_{org,name}
 }
 
 main "$@"

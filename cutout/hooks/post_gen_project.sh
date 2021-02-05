1650a91d0fedd591e9c7def7880aa5b4f4a99e7c
Amanibhavam <iam@defn.sh>
2021-02-04 19:22:38 -0800

    fake cutout using sed

---
+++ cutout/hooks/post_gen_project.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+function main {
+  set -efu
+  find static -type f | while read -r f; do sed -i '' "s#defn#$(cat .project_org)#g; s#podinfo#$(cat .project_name)#g" "$f"; done
+  pushd static
+  mv charts/podinfo "charts/$(cat ../.project_name)"
+  mv cmd/podinfo "cmd/$(cat ../.project_name)"
+  popd
+  rsync -ia static/. "$(cat .project_name)/."
+  rm -rf static
+  rm -f "$(cat .project_name)"/.project_{org,name}
+}
+
+main "$@"

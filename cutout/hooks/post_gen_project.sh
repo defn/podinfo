63d545c9a5512c9de72476dc08f404b6cdb495d7
Amanibhavam <iam@defn.sh>
2021-02-04 19:37:00 -0800

    debug

--- cutout/hooks/post_gen_project.sh
+++ cutout/hooks/post_gen_project.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function main {
-  set -efu
+  set -exfu
   find static -type f | while read -r f; do sed -i '' "s#defn#$(cat .project_org)#g; s#podinfo#$(cat .project_name)#g" "$f"; done
   pushd static
   mv charts/podinfo "charts/$(cat ../.project_name)" || true
